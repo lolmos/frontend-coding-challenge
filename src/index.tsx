4143dddd1dcf4d4d5fccfdc4561e1472538eed97
lolmos <luis.olmos@cybrik.ai>
2022-08-12 19:21:06 +0200

    start

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import GlobalStyle from './GlobalStyle';
-import store from './store';
+import Button from './components/Button';
 import Container from './components/Container';
 import H4 from './components/H4';
+import Input from './components/Input';
+import GlobalStyle from './GlobalStyle';
+import store from './store';
 
 const App = () => {
+  // const tournaments = useSelector()
+  const handleSearch = (e: React.MouseEvent<HTMLInputElement>) => {
+    const tournament = e.target;
+    console.log('toournament', tournament);
+  };
   return (
     <Container>
       <H4>FACEIT Tournaments</H4>
+      <Input placeholder="Search tournament ..." name="tournament" />
+      <Button>CREATE TOURNAMENT</Button>
     </Container>
   );
 };
