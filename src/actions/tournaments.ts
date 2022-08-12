559daa9ef710258e85f8345d73a6770a62d8e854
lolmos <luis.olmos@cybrik.ai>
2022-08-12 20:59:05 +0200

    start redux

--- src/actions/tournaments.ts
+++ src/actions/tournaments.ts
@@ -1,1 +1,35 @@
-import { RootState } from '../reducers';
+import { API_TOURNAMENTS_URL } from '../constants/api';
+import { Tournament } from '../reducers/tournaments';
+
+export const getTournaments = () => {
+  return fetch(API_TOURNAMENTS_URL)
+    .then(handleErrors)
+    .then((res) => res.json());
+};
+
+// export const fetchTournaments()
+
+const handleErrors = (res: any) => {
+  if (!res.ok) {
+    throw Error(res.statusText);
+  }
+  return res;
+};
+
+export const FETCH_TOURNAMENTS_BEGIN = 'FETCH_TOURNAMENTS_BEGIN';
+export const FETCH_TOURNAMENTS_SUCCESS = 'FETCH_TOURNAMENTS_SUCCESS';
+export const FETCH_TOURNAMENTS_FAILURE = 'FETCH_TOURNAMENTS_FAILURE';
+
+export const fetchTournamentsBegin = () => ({
+  type: FETCH_TOURNAMENTS_BEGIN,
+});
+
+export const fetchTournamentsSuccess = (tournaments: Tournament[]) => ({
+  type: FETCH_TOURNAMENTS_SUCCESS,
+  payload: { tournaments },
+});
+
+export const fetchTournamentsFailure = (error: any) => ({
+  type: FETCH_TOURNAMENTS_FAILURE,
+  payload: { error },
+});
--- src/reducers/tournaments.ts
+++ src/reducers/tournaments.ts
@@ -1,8 +1,36 @@
+export interface Tournament {
+  id: string;
+  name: string;
+  organizer: string;
+  game: string;
+  participants: {
+    current: number;
+    max: number;
+  };
+  startDate: Date;
+}
+
 const initialState = {};
+
+// export const tournamentTypes = {
+//   FETCH_TOURNAMENTS: "FETCH_TOURNAMENTS"
+// }
+
+const FETCH_TOURNAMENTS = 'FETCH_TOURNAMENTS';
+
+interface Actions {
+  type: typeof FETCH_TOURNAMENTS | unknown;
+  payload: any;
+}
 
 export default function tournaments(
   state: unknown = initialState,
-  action: unknown
+  action: Actions
 ) {
-  return state;
+  switch (action.type) {
+    case FETCH_TOURNAMENTS:
+      return state;
+  }
+
+  // return state;
 }
