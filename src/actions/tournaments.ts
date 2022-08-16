235ff8b0fd26d5f98e7f0e86e980fdd11da49a3e
lolmos <luis.olmos@cybrik.ai>
2022-08-16 19:03:18 +0200

    getting the actions ready for deleting

--- src/actions/tournaments.ts
+++ src/actions/tournaments.ts
@@ -1,13 +1,27 @@
+import { Dispatch } from 'redux';
 import { API_TOURNAMENTS_URL } from '../constants/api';
 import { Tournament } from '../reducers/tournaments';
 
 export const getTournaments = () => {
+  console.log('fetching');
   return fetch(API_TOURNAMENTS_URL)
     .then(handleErrors)
     .then((res) => res.json());
 };
 
-// export const fetchTournaments()
+export const fetchTournaments = () =>
+  // dispatch:Dispatch
+  {
+    console.log('getting to here??');
+    return (dispatch: Dispatch) => {
+      dispatch(fetchTournamentsStart());
+      return getTournaments()
+        .then((json) => {
+          dispatch(fetchTournamentsSuccess(json));
+        })
+        .catch((err) => dispatch(fetchTournamentsFailure(err)));
+    };
+  };
 
 const handleErrors = (res: any) => {
   if (!res.ok) {
@@ -16,12 +30,16 @@ const handleErrors = (res: any) => {
   return res;
 };
 
-export const FETCH_TOURNAMENTS_BEGIN = 'FETCH_TOURNAMENTS_BEGIN';
+export const FETCH_TOURNAMENTS_START = 'FETCH_TOURNAMENTS_START';
 export const FETCH_TOURNAMENTS_SUCCESS = 'FETCH_TOURNAMENTS_SUCCESS';
 export const FETCH_TOURNAMENTS_FAILURE = 'FETCH_TOURNAMENTS_FAILURE';
 
-export const fetchTournamentsBegin = () => ({
-  type: FETCH_TOURNAMENTS_BEGIN,
+export const DELETE_TOURNAMENT_START = 'DELETE_TOURNAMENT_START';
+export const DELETE_TOURNAMENT_SUCCESS = 'DELETE_TOURNAMENT_SUCCESS';
+export const DELETE_TOURNAMENT_FAILURE = 'DELETE_TOURNAMENT_FAILURE';
+
+export const fetchTournamentsStart = () => ({
+  type: FETCH_TOURNAMENTS_START,
 });
 
 export const fetchTournamentsSuccess = (tournaments: Tournament[]) => ({
